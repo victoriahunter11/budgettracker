a9e8dc23986acfabab5f876e82d535325099b50a
Victoria Hunter <victoria.jane09@gmail.com>
2021-01-20 18:22:38 -0500

    adding service worker file and manifest

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+ ];
